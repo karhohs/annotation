a19f0b2dcf8ec7183184c5a9aa60034fa079c221
Allen Goodman <allen.goodman@icloud.com>
2017-01-17 15:39:18 -0500

    scaffolding

---
+++ src/Application.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './Application.css';
+
+class Application extends Component {
+  render() {
+    return (
+      <div className="Application">
+
+      </div>
+    );
+  }
+}
+
+export default Application;
